37401e59bf6c9169ae123692b9a947c4c1b070b2
Poorna Chandran <cpoorna414@gmail.com>
2026-02-03 10:20:38 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,18 @@
+package Coreproject;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		 
+		int a=5 ;
+		 int b=10;
+		    
+		    a=a+b;
+		    b=a-b;
+		    a=a-b;
+		    System.out.print("  a= "+ a +
+		    		            " b= " + b );
+		    
+		    }
+
+}
